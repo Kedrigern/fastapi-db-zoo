442644c8a4d0ff7675f618cfa14680da290a1dac
Ondřej Profant <ondrej.profant@gmail.com>
2025-03-30 21:16:32 +0200

    Config file

--- src/database/connection.py
+++ src/database/connection.py
@@ -1,9 +1,11 @@
 from typing import Iterator
 from sqlmodel import SQLModel, Session, create_engine
 
+from src.config import config
+
 
 connect_args = {"check_same_thread": False}
-engine = create_engine("sqlite:///test.db", echo=True, connect_args=connect_args)
+engine = create_engine(config.DB, echo=True, connect_args=connect_args)
 
 
 def create_db_and_tables() -> None:
